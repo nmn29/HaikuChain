76c8ac07abad42b06eef5f2b7e991974a03d25ef
nmn29 <ina010218@gmail.com>
2023-02-15 14:04:00 +0900

    upload file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getStorage } from 'firebase/storage';
 import { getAuth, setPersistence, inMemoryPersistence } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCSwrP9YtA0RyTNv6JN58ZMBuLbpFEAvog",
-  authDomain: "haikuchain.firebaseapp.com",
-  projectId: "haikuchain",
-  storageBucket: "haikuchain.appspot.com",
-  messagingSenderId: "295417598691",
-  appId: "1:295417598691:web:28f5b2e2ace5b328d5abd6",
-  measurementId: "G-5W05XZQ40X"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
